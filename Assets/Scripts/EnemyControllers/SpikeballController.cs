b1c3257aad8c15b7fc0f72fb4d8e23a8e4f64b9b
alexander89162 <alexander89162@gmail.com>
2026-04-02 09:47:25 -0700

    Refined structure for spikeball AI, now begins charging. work in progress

--- Assets/Scripts/EnemyControllers/SpikeballController.cs
+++ Assets/Scripts/EnemyControllers/SpikeballController.cs
@@ -6,18 +6,23 @@ AI: 3 stages; move near player, rising into air/launching self, and recharge
 Animation: basic roll on the ground, spin slowly when lifting and fast when launched*/
 public class SpikeballController : MonoBehaviour
 {
-    [Header("References")]
-    public NavMeshAgent agent;
-
+    public Transform enemyTarget;
     public float movementSpeed = 20f;
     public float repathInterval = 0.8f;
-    public float liftingTime = 2f;
+    public float moveStep = 20f;
+    public float minChargeDistance = 40f;
+    public float liftingTime = 4f;
     public float liftingHeight = 30f;
     public float launchRechargeTime = 3f;
     public float radius = 9f;
 
+    private NavMeshAgent agent;
     private AttackState currentState;
+    private Vector3 moveDestination;
+    private Vector3 liftingStartPos;
+    private Vector3 liftingEndPos;
     private float repathTimer = 0f;
+    private float liftingTimer = 0f;
     private Vector3 lastPos;
 
     public bool debug = false;
@@ -40,23 +45,57 @@ public class SpikeballController : MonoBehaviour
     {
         repathTimer += Time.deltaTime;
 
-
-        if (repathTimer > repathInterval)
+        // manually handle all time-sensitive states, if none of these then go to general Repath()
+        if (currentState == AttackState.Lifting)
+        {
+            liftingTimer += Time.deltaTime;
+            if (liftingTimer > liftingTime)
+            {
+                SetState(AttackState.Launching);
+                liftingTimer = 0f;
+            }
+            else
+            {
+                //
+            }
+        }
+        else if (currentState == AttackState.Launching)
+        {
+            //
+        }
+        else if (repathTimer > repathInterval)
         {
             Repath();
             repathTimer = 0f;
         }
 
+        agent.SetDestination(moveDestination);
         AnimateSelf();
     }
 
     private void Repath()
     {
-        switch (currentState)
+        switch (currentState) // some states are handled in Update() instead of here since they're more time-sensitive
         {
-            case AttackState.Approaching: break;
-            case AttackState.Lifting: break;
-            case AttackState.Launching: break;
+            case AttackState.Approaching:
+                Vector3 toEnemy = enemyTarget.position - transform.position;
+                Vector3 toSelf = -toEnemy;
+
+                float sideDot = Vector3.Dot(toSelf.normalized, enemyTarget.right);
+                float orbitSign = sideDot >= 0f ? 1f : -1f;
+                Vector3 sidePoint = enemyTarget.position + enemyTarget.right * (orbitSign * minChargeDistance);
+
+                if (Vector3.Distance(transform.position, sidePoint) < minChargeDistance)
+                {
+                    liftingStartPos = transform.position;
+                    liftingEndPos = liftingStartPos + new Vector3(0, liftingHeight, 0);
+                    SetState(AttackState.Lifting);
+                    break;
+                }
+                Vector3 desiredDir = (sidePoint - transform.position).normalized;
+                Vector3 desiredTarget = transform.position + (desiredDir * moveStep);
+                moveDestination = desiredTarget;
+                break;
         }
     }
 
