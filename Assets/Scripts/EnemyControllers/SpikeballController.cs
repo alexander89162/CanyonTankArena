d0ae422dadb1114a3e0c3e51948868f66c2c5967
Alexander <alexander891622gmail.com>
2026-04-02 21:07:48 -0700

    Improved tank tread color, added support for spikeball to lift into air and launch. currently very buggy

--- Assets/Scripts/EnemyControllers/SpikeballController.cs
+++ Assets/Scripts/EnemyControllers/SpikeballController.cs
@@ -7,6 +7,7 @@ Animation: basic roll on the ground, spin slowly when lifting and fast when laun
 public class SpikeballController : MonoBehaviour
 {
     public Transform enemyTarget;
+    public Transform spikeballRenderer;
     public float movementSpeed = 20f;
     public float repathInterval = 0.8f;
     public float moveStep = 40f;
@@ -14,6 +15,10 @@ public class SpikeballController : MonoBehaviour
     public float liftingTime = 4f;
     public float liftingHeight = 30f;
     public float launchRechargeTime = 3f;
+    public float launchStrengthMultiplier = 1f;
+    public float gravityMultiplier = 1f;
+    public float exitFreeRoamVel = 3f; // when velocity drops this amount, exit FreeRoam state and return to NavMesh
+    public float minFreeRoamTime = 0.5f;
     public float radius = 9f;
 
     private NavMeshAgent agent;
@@ -24,15 +29,16 @@ public class SpikeballController : MonoBehaviour
     private Vector3 liftingEndPos;
     private float repathTimer = 0f;
     private float liftingTimer = 0f;
+    private float freeRoamTimer = 0f;
     private Vector3 lastPos;
 
     public bool debug = false;
 
     public enum AttackState
     {
-        Approaching,
-        Lifting,
-        Launching
+        Approaching, // roll towards target with NavMesh
+        Lifting, // Lerp position
+        FreeRoam // free roam with physics using rigidbody until most momentum is lost (velocity < exitFreeRoamVel). This state handles both launching and bouncing
     }
 
     void Awake()
@@ -40,6 +46,7 @@ public class SpikeballController : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         rb.isKinematic = true; // rb only used for Launching state
+        rb.useGravity = true;
         moveDestination = transform.position;
 
         SetState(AttackState.Approaching);
@@ -49,7 +56,8 @@ public class SpikeballController : MonoBehaviour
     {
         repathTimer += Time.deltaTime;
 
-        // manually handle all time-sensitive states, if none of these then go to general Repath()
+        /* Manually handle all time-sensitive states first, if none of 
+        these then go to general Repath()*/
         if (currentState == AttackState.Lifting)
         {
             liftingTimer += Time.deltaTime;
@@ -59,25 +67,37 @@ public class SpikeballController : MonoBehaviour
 
             if (t >= 1)
             {
-                SetState(AttackState.Launching);
-                liftingTimer = 0f;
+                SetState(AttackState.FreeRoam);
             }
         }
-        else if (currentState == AttackState.Launching)
+        else if (currentState == AttackState.FreeRoam)
         {
-            //
+            freeRoamTimer += Time.deltaTime;
+
+            if (freeRoamTimer >= minFreeRoamTime)
+            {
+                float currentVelocity = Vector3.Distance(transform.position, lastPos) / Time.deltaTime;
+                if (currentVelocity < exitFreeRoamVel)
+                {
+                    if (debug) Debug.Log($"Exited {currentState} because the spikeball was moving below {exitFreeRoamVel} m/s.");
+                    SetState(AttackState.Approaching);
+                }
+            }
         }
-        else if (repathTimer > repathInterval)
+        else if (repathTimer > repathInterval) // general Repath case
         {
             Repath();
-            repathTimer = 0f;
         }
 
+        lastPos = transform.position;
         AnimateSelf();
     }
 
     private void Repath()
     {
+        repathTimer = 0f;
+        if (debug) Debug.Log($"Repath() called when in the {currentState} state");
+        
         switch (currentState) // some states are handled in Update() instead of here since they're more time-sensitive
         {
             case AttackState.Approaching:
@@ -111,16 +131,42 @@ public class SpikeballController : MonoBehaviour
                 float distanceTraveled = Vector3.Distance(transform.position, lastPos);
                 float rotationAngle = distanceTraveled / radius * Mathf.Rad2Deg;
                 Vector3 moveDir = (transform.position - lastPos).normalized;
-                transform.Rotate(Vector3.Cross(moveDir, Vector3.up), -rotationAngle, Space.World);
+                spikeballRenderer.Rotate(Vector3.Cross(moveDir, Vector3.up), -rotationAngle, Space.World);
                 break;
             case AttackState.Lifting: break;
-            case AttackState.Launching: break;
+            case AttackState.FreeRoam: break;
         }
-        lastPos = transform.position;
     }
 
+    ///<summary>Set the current state and handle all state changes for the specified transition</summary>
     public void SetState(AttackState newState)
     {
+        switch (newState)
+        {
+            case AttackState.Approaching:
+                agent.enabled = true;
+                rb.isKinematic = true;
+                repathTimer = 0f;
+                break;
+            case AttackState.Lifting:
+                agent.enabled = false;
+                liftingTimer = 0f;
+                break;
+            case AttackState.FreeRoam:
+                // Deactivate NavMesh agent and prepare RigidBody
+                agent.enabled = false;
+                rb.isKinematic = false;
+                rb.linearVelocity = Vector3.zero;
+                freeRoamTimer = 0f;
+
+                // Find direction to player, aim and launch
+                Vector3 toTarget = (enemyTarget.position - transform.position).normalized;
+                Vector3 launchDir = new Vector3(toTarget.x, toTarget.y + 1.2f, toTarget.z).normalized;
+                rb.AddForce(launchDir * rb.mass * launchStrengthMultiplier, ForceMode.Impulse);
+
+                break;
+        }
+
         if (debug) Debug.Log($"Changed from {currentState} to {newState}");
         currentState = newState;
     }
