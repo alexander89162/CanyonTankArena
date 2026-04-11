d87c4655e2f907c38a494f0205feaba9078b955e
Alexander <alexander89162@gmail.com>
2026-04-11 11:05:58 -0700

    small changes to rig, and added rest pose caching

--- Assets/Scripts/AnimationScripts/WeaponAimers/CannonAimer.cs
+++ Assets/Scripts/AnimationScripts/WeaponAimers/CannonAimer.cs
@@ -5,6 +5,16 @@ public class CannonAimer : WeaponAimer
     [SerializeField] private Transform cannonBody;
     [SerializeField] private Transform cannonBarrel;
 
+    private Quaternion bodyRestRotation;
+    private Quaternion barrelRestRotation;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        bodyRestRotation   = cannonBody.rotation;
+        barrelRestRotation = cannonBarrel.rotation;
+    }
+
     public override void AimAt(Vector3 worldTarget)
     {
         if (!weaponEnabled) return;
@@ -14,8 +24,8 @@ public class CannonAimer : WeaponAimer
         float yaw   = Mathf.Atan2(toEnemy.x, toEnemy.z) * Mathf.Rad2Deg;
         float pitch = -Mathf.Atan2(toEnemy.y, new Vector2(toEnemy.x, toEnemy.z).magnitude) * Mathf.Rad2Deg;
 
-        cannonBody.rotation   = Quaternion.Euler(0, yaw, 0);
-        cannonBarrel.rotation = Quaternion.Euler(pitch, yaw, 0);
+        cannonBody.rotation   = bodyRestRotation   * Quaternion.Euler(0, yaw, 0);
+        cannonBarrel.rotation = barrelRestRotation * Quaternion.Euler(pitch, 0, 0);
     }
 
     public override void Fire()
