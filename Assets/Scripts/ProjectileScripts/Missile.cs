abc1232987fbff78c6a6901b633ce0b0971ba9c4
alexander89162 <alexander89162@gmail.com>
2026-04-22 22:41:57 -0700

    Made missiles collide properly and deal damage to nearby units. Also tweaked the missile launcher to launch missiles at a nicer angle and configured a missile prefab

--- Assets/Scripts/ProjectileScripts/Missile.cs
+++ Assets/Scripts/ProjectileScripts/Missile.cs
@@ -1,13 +1,30 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+/*Drives the missile's physics simulation and handles explosion upon 
+collision by its gameObject's RigidBody component.*/
 public class Missile : MonoBehaviour
 {
     public float lifeTime = 10f;
+    public GameObject explosionPrefab;
+    public LayerMask triggerExplosionMask;
+    public LayerMask damageMask;
+    public float explosionDamage = 20f;
+    public float explosionRadius = 20f;
+
+    private Rigidbody rb;
     private Vector3 currentVelocity;
     private float gravityMultiplier = 9.81f;
     private float forwardAcceleration;
     private float lifetimeRemaining = -1f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        int ownerLayer = gameObject.layer;
+        damageMask &= ~(1 << ownerLayer); // no damage to self
+    }
 
     void Update()
     {
@@ -18,7 +35,7 @@ public class Missile : MonoBehaviour
         // 1) Handle movement
         currentVelocity += currentVelocity.normalized * forwardAcceleration * Time.deltaTime;
         currentVelocity += Vector3.down * gravityMultiplier * Time.deltaTime;
-        transform.position += currentVelocity * Time.deltaTime;
+        rb.MovePosition(rb.position + currentVelocity * Time.deltaTime);
 
         // 2) adjust orientation to match movement
         transform.rotation = Quaternion.LookRotation(currentVelocity);
@@ -33,5 +50,36 @@ public class Missile : MonoBehaviour
 
         currentVelocity = transform.forward * missileLaunchSpeed;
         lifetimeRemaining = lifeTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int hitLayer = collision.gameObject.layer;
+
+        bool shouldExplode = (triggerExplosionMask.value & (1 << hitLayer)) != 0;
+
+        if (shouldExplode)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        // 1) Spawn explosion prefab
+        if (explosionPrefab != null)
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        // 2) Apply damage to nearby enemies
+        Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius, damageMask);
+
+        foreach (var hit in hits)
+        {
+            DamageController damage = hit.GetComponentInParent<DamageController>();
+            if (damage != null) 
+                damage.TakeDamage(explosionDamage);
+        }
+
+        Destroy(gameObject);
     }
 }
